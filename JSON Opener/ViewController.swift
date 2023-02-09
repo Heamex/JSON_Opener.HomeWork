6fc164b0c7552bdd69d2c9b9473c3553ba81e73e
Heamex <1909.belov@gmail.com>
2023-02-09 20:48:57 +0300

    Initial Commit

---
+++ JSON Opener/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON Opener
+//
+//  Created by Nikita Belov on 09.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
